2eb96e91d10a04cc059aafa533e0a8ea362543a0
Nabin Sahoo <79052753+sabotage-py@users.noreply.github.com>
2023-07-21 11:21:18 +0530

    handle more alethe rules

--- lib/translate.ml
+++ lib/translate.ml
@@ -1,18 +1,11 @@
 open Ast
+module H = Helper
 
-let join_with s list = 
-  let rec aux str = function
-  | [] -> str
-  | x :: t -> match str with 
-  | "" -> aux x t
-  | _ -> aux (str ^ s ^ x) t
-in aux "" list
+type l_term = string 
 
-(* let map f a = 
-  let rec aux b = function
-  | [] -> b
-  | x :: t -> aux ((f x) :: b) t
-in aux [] (List.rev a);; *)
+exception SyntaxError of string
+exception SemanticError of string
+exception UnderConstruction of string  (*used while working on the code*)
 
 let rec term_translate = function
 | Symbol x -> let sym = match x with 
@@ -22,142 +15,86 @@ let rec term_translate = function
 | Const c -> let string_of_const = function | Numeral y -> string_of_int y | String y -> y in 
   string_of_const c
 | Not t -> "¬ " ^ term_translate t
-| Or list -> "(" ^ join_with " ∨c " (List.map term_translate list) ^ ")"
-| And list -> "(" ^ join_with " ∧c " (List.map term_translate list) ^ ")"
-| Equal (l, r) -> "(" ^ term_translate l ^ " = " ^ term_translate r ^ ")"
+| Or list -> "(" ^ H.join_with " ∨c " (List.map term_translate list) ^ ")"
+| And list -> "(" ^ H.join_with " ∧c " (List.map term_translate list) ^ ")"
+| Xor list -> "(" ^ H.join_with " xorc " (List.map term_translate list) ^ ")"
+| Equal (l, r) -> "(" ^ term_translate l ^ " =c " ^ term_translate r ^ ")"
+| Implies (l, r) -> "(" ^ term_translate l ^ " ⇒c " ^ term_translate r ^ ")"
 | _ -> "TODO"
 
-(* let rec merge = 
-  let rec merge_two_lists a b = 
-    let rec is_it_in list n = match list with 
-    | [] -> false
-    | x :: t -> if x = n then true else is_it_in t n in 
-  match b with 
-  | [] -> a
-  | x :: t -> if is_it_in a x then merge_two_lists a t else merge_two_lists (a @ [x]) t in
-  function
-| [] -> []
-| [] :: t -> merge t
-| [a] -> a
-| a :: b :: t -> merge ((merge_two_lists a b) :: t) *)
-
-(* let rec get_subsymbols = function
-| Symbol x -> [x]
-| Const c -> let string_of_const = function | Numeral y -> string_of_int y | String y -> y in
-  [string_of_const c] (*do something else for the constants*)
-| Not t -> get_subsymbols t
-| Or list | And list -> merge (map get_subsymbols list)
-| Equal (l, r) -> merge [get_subsymbols l; get_subsymbols r]
-| _ -> ["TODO"] *)
-
-exception SyntaxError of string
-exception SemanticError of string
-exception UnderConstruction of string
-
-(* returns the index of el in list *)
-let rec find_idx el = function 
-| [] -> raise Not_found
-| x :: t -> if x = el then 0 else 1 + find_idx el t 
-
-let rec find_idx_opt el = function
-| [] -> None
-| x :: t -> if x = el then Some 0 else 
-  match find_idx_opt el t with
-  | None -> None
-  | Some i -> Some (1 + i)
-
-let unique_list list =
-  let rec aux keeper = function 
-  | [] -> keeper
-  | x :: t -> match find_idx_opt x keeper with 
-    | None -> aux (x :: keeper) t
-    | _ -> aux keeper t in 
-  List.rev (aux [] list)
-
-let elim_last list = 
-  let rec aux keeper = function 
-  | [] | [_] -> keeper
-  | x :: t -> aux (x :: keeper) t in 
-  List.rev (aux [] list)
-
-let rec rem_el el = function 
-| [] -> []
-| x :: t -> if x = el then t else (x :: (rem_el el t))
-
-(* let find_idx el list = List.find 
-  (function | -1 -> false | _ -> true)
-  (
-    List.mapi (fun i r -> if r = el then i else -1) list
-  ) *)
-
-(* let rec get_str_n str = function 
-| 0 -> ""
-| n -> str ^ get_str_n str (n - 1) *)
-
-let l_prf x = "Prf " ^ x 
-let l_prfc x = "Prfc " ^ x
-let l_assume x = "assume " ^ join_with " " x ^ "; "
-let l_refine  x = "refine " ^ join_with " " x ^ "; "
-let l_apply x = "apply " ^ join_with " " x ^ "; "
-let l_have (x : string) (proof : string) (list : string list) : string = 
-  "have " ^ x ^ " : " ^ proof ^ " { \n" ^ join_with "\n" list ^ "};\n"
-let l_app x y = x ^ " (" ^ y ^ ")"
-let l_proof_statement proof list = proof ^ " ≔ begin \n" ^ 
-  join_with " \n" list ^ " \nend;\n"
+let l_prf (x : l_term) : l_term = "Prf " ^ x 
+let l_prfc (x : l_term) : l_term = "Prfc " ^ x
+let l_assume (x : l_term list) : l_term = "assume " ^ H.join_with " " x ^ "; "
+let l_refine  (x : l_term list) : l_term = "refine " ^ H.join_with " " x ^ "; "
+let l_apply (x : l_term list) : l_term = "apply " ^ H.join_with " " x ^ "; "
+let l_have (x : l_term) (ltype : l_term) (list : l_term list) : l_term = 
+  "have " ^ x ^ " : " ^ ltype ^ " { \n" ^ H.join_with "\n" list ^ "};\n"
+let l_app (x : l_term) (y : l_term) : l_term = x ^ " (" ^ y ^ ")"
+let l_proof_statement (ltype : l_term) (list : l_term list) : l_term = 
+  (*returns the type and its proof*)
+  ltype ^ " ≔ begin \n" ^ H.join_with " \n" list ^ " \nend;\n"
 let ltop = "⊤"
 let lbot = "⊥"
 let lnot = "¬"
-let limpliesc x y = "(" ^ x ^ " ⇒c " ^ y ^ ")"
-let leqc x y = "(" ^ x ^ " =c " ^ y ^ ")"
+let limpliesc x y = 
+  "(" ^ x ^ " ⇒c " ^ y ^ ")"
+let leqc x y = 
+  "(" ^ x ^ " =c " ^ y ^ ")"
 
-let apply_n_times (n : int) (app_str : string) (init_str : string) : string = 
-  List.fold_left (fun y _ -> l_app app_str y) init_str (List.init n (fun _ -> 0))
+let apply_n_times (n : int) (app : l_term) (init0 : l_term) : l_term =
+  (*returns app (app (... n times ... app ( init0 )))*) 
+  List.fold_left (fun y _ -> l_app app y) init0 (List.init n (fun _ -> 0))
 
-let rec not_counter = function 
+let rec not_counter = function
+(*checks the number of 'Not's in an alethe term;
+  returns that number and the outer-most term that is not a 'Not'*)
 | Not z -> let (p, fi) = not_counter z in (1 + p, fi) 
 | z -> (0, z)
 
 let and_repetition_remover (xlist : term list) (ylist : term list): string = 
+  (*returns the proof of Prfc (And (xlist) =c And (ylist)), 
+    where ylist has all repetitions removed and  
+    by first getting the proofs for implication in both directions *)
   let len_xlist = List.length xlist in
   let len_ylist = List.length ylist in
   let xt = term_translate (And xlist) in 
   let yt = term_translate (And ylist) in
-  let rec get_indices (alist : term list) (blist : term list) : int option list = 
-    match alist with 
-    | x :: t -> find_idx_opt x blist :: (get_indices t blist)
-    | [] -> [] in 
-  let indices = List.map (function | None -> -1 | Some i -> i) (get_indices ylist xlist) in 
-  let extract_and_elim hyp_name p i = 
-    l_have ("a" ^ string_of_int p) (l_prfc (term_translate (List.nth ylist p))) [
-      let selector = apply_n_times i "∧e2c _ _" hyp_name in 
-      if i = len_xlist - 1 then l_refine [selector] else l_refine [l_app "∧e1c _ _" selector]] in
-  let extract_and_elim_subproof hyp_name = List.mapi (extract_and_elim hyp_name) indices in
-  let combine_formula = List.fold_right (fun p x -> l_app ("∧ic _ _ a" ^ string_of_int p) x) (List.init (len_ylist - 1) (fun x -> x)) ("a" ^ string_of_int (len_ylist - 1)) in
-  let combine_subproof = l_have "h1" (l_prf (limpliesc xt yt)) 
-    ([l_assume ["g0"]] @ (extract_and_elim_subproof "g0") @ [l_refine [combine_formula]]) in
-  let orig_indices2 = List.map (function | None -> len_ylist | Some i -> i) (get_indices xlist ylist) in 
-  let indices2 = rem_el len_ylist (unique_list (orig_indices2)) in 
-  let extract_and_elim2 hyp_name i = 
+  (*First, get proof of Prf (xt ⇒c yt)*)
+  let indices = List.map (function | None -> -1 | Some i -> i) (H.get_indices ylist xlist) in 
+  let have_ith_element_of_ylist_from_xlist hyp_name i p = 
+    l_have ("a" ^ string_of_int i) (l_prfc (term_translate (List.nth ylist i))) [
+      let selector = apply_n_times p "∧e2c _ _" hyp_name in 
+      if p = len_xlist - 1 then l_refine [selector] else l_refine [l_app "∧e1c _ _" selector]] in
+  (*have ai : Prfc (ith element of ylist) {...};*)
+  let have_elements_of_ylist hyp_name = List.mapi (have_ith_element_of_ylist_from_xlist hyp_name) indices in
+  let have_yt = List.fold_right (fun p x -> l_app ("∧ic _ _ a" ^ string_of_int p) x) (List.init (len_ylist - 1) Fun.id) ("a" ^ string_of_int (len_ylist - 1)) in
+  let have_xt_implies_yt = l_have "h1" (l_prf (limpliesc xt yt)) 
+    ([l_assume ["g0"]] @ (have_elements_of_ylist "g0") @ [l_refine [have_yt]]) in
+  (*Now, get proof of Prf (yt ⇒c xt)*)
+  let orig_indices = List.map (function | None -> len_ylist | Some i -> i) (H.get_indices xlist ylist) in 
+  let indices2 = H.rem_el len_ylist (H.unique_list (orig_indices)) in 
+  let have_ith_element_of_ylist_from_ylist hyp_name i = 
     l_have ("a" ^ string_of_int i) (l_prfc (term_translate (List.nth ylist i))) [
       let selector = apply_n_times i "∧e2c _ _" hyp_name in 
       if i = len_ylist - 1 then l_refine [selector] else l_refine [l_app "∧e1c _ _" selector]] in 
-  let extract_and_elim_subproof2 hyp_name = List.map (extract_and_elim2 hyp_name) indices2 in
-  let combine_formula2 = List.fold_right (fun p x -> l_app ("∧ic _ _ a" ^ string_of_int p) x) (elim_last orig_indices2) ("a" ^ string_of_int (List.hd (List.rev orig_indices2))) in
-  let combine_subproof2 = l_have "h2" (l_prf (limpliesc yt xt)) ([l_assume ["k0"]] @ (extract_and_elim_subproof2 "k0") @ 
+  let have_elements_of_ylist_from_ylist hyp_name = List.map (have_ith_element_of_ylist_from_ylist hyp_name) indices2 in
+  let have_xt = List.fold_right (fun p x -> l_app ("∧ic _ _ a" ^ string_of_int p) x) (H.elim_last orig_indices) ("a" ^ string_of_int (List.hd (List.rev orig_indices))) in
+  let have_yt_implies_xt = l_have "h2" (l_prf (limpliesc yt xt)) ([l_assume ["k0"]] @ (have_elements_of_ylist_from_ylist "k0") @ 
     [l_have ("a" ^ string_of_int (len_ylist)) (l_prfc ltop) [l_refine ["intuition _"; "true"]];
-    l_refine [combine_formula2]]) in
+    l_refine [have_xt]]) in
+  (*Final step*)
   l_proof_statement (l_prfc (leqc xt yt)) [
-    l_assume ["nh"]; l_refine ["nh _"]; combine_subproof; combine_subproof2;
+    l_assume ["nh"]; l_refine ["nh _"]; have_xt_implies_yt; have_yt_implies_xt;
     l_assume ["s1"; "left1"]; l_apply ["left1"; "h1"; "h2"]
   ]
 
 let rule_and_simplify x y = 
   match x with 
-  | Ast.And xlist -> let len_xlist = List.length xlist in 
+  | And xlist -> let len_xlist = List.length xlist in 
   let t = term_translate (And xlist) in
   let lpi_proof = match y with 
-    | Ast.And ylist -> and_repetition_remover xlist ylist
-    | Ast.Symbol "false" -> let rec get_contradicting_formulae = function 
+    | And ylist -> and_repetition_remover xlist ylist
+    | Symbol "false" -> let rec get_contradicting_formulae = function 
       | [_] | [] -> raise (SemanticError "there is no contradiction in LHS for RHS to be 'false'")
       | x :: tl -> let rec aux2 (p, f) = function 
         | z :: sl -> let (q, g) = not_counter z in 
@@ -172,27 +109,32 @@ let rule_and_simplify x y =
       | x :: tl -> let (p, f) = not_counter x in 
         if f = Symbol "false" && p mod 2 = 0 then Some (p, f, i) else check_false (i + 1) tl in 
       let proof_of_contradiction = match check_false 0 xlist with 
-      | Some (p, f, i) -> l_proof_statement (l_prfc ("(" ^ t ^ " =c ⊥)"))
+      (*check if ⊥ is in xlist*)
+      | Some (p, f, i) -> l_proof_statement (l_prfc (leqc t lbot))
         [
           l_assume ["nh"]; l_refine ["nh _"];
-          l_have "h1" (l_prf ("(" ^ t ^ " ⇒c ⊥)")) [
+          l_have "h1" (l_prf (limpliesc t lbot)) [
             l_assume ["h3"]; l_have "f1" (l_prfc (apply_n_times p lnot (term_translate f))) [
               let selector = apply_n_times i "∧e2c _ _" "h3" in 
               if i = len_xlist - 1 then l_refine [selector] else l_refine [l_app "∧e1c _ _" selector]; 
             ];
             l_refine [apply_n_times (p / 2) "double_neg _" "f1"];
           ];
-          l_have "h2" (l_prf ("(⊥ ⇒c " ^ t ^ ")")) [
+          l_have "h2" (l_prf (limpliesc lbot t)) [
             l_assume ["nnf"]; l_apply ["⊥e"]; l_apply ["nnf"];
             l_assume ["pf"]; l_refine ["pf"];
           ];
           l_assume ["s1"; "left1"]; l_apply ["left1"; "h1"; "h2"]
         ]
-      | None -> 
+      | None -> (*⊥ is not in xlist; we must have two contradicting formulae*)
       let (x, j1, z, j2) = get_contradicting_formulae xlist in 
+      (*get x, z from the list such that 
+        x = not not ... phi, z = not not ... phi 
+        and x ∧ z = ⊥*)
       let (f1, f2, i1, i2) = if j1 < j2 then (x, z, j1, j2) else (z, x, j2, j1) in 
-      let f1i = find_idx f1 xlist in
-      let f2i = find_idx f2 xlist in 
+      (*f2 has more 'not's than f1*)
+      let f1i = H.find_idx f1 xlist in
+      let f2i = H.find_idx f2 xlist in 
       let f1_header = apply_n_times f1i "∧e2c _ _" "h3" in 
       let f2_header = apply_n_times f2i "∧e2c _ _" "h3" in
       (* let len_xlist = List.length xlist in *)
@@ -203,28 +145,33 @@ let rule_and_simplify x y =
       let aux_foo str n = 
         let sn = string_of_int n in 
         str ^ l_assume ["f2_" ^ sn] ^ l_refine ["f2_" ^ sn; "_"] ^ "\n" in 
-      let h1_footer = List.fold_left aux_foo "refine f2 _; \n" (List.init ((i2 - i1) / 2) (fun r -> r + 1)) ^ "refine f1; \n" in
-      let h1_body = l_have "h1" (l_prf ("(" ^ t ^ " ⇒c ⊥)")) 
-        [l_assume ["h3"; "nf"]; l_refine ["h3"; "_"]; l_assume ["h4"]; have_f1; have_f2; h1_footer;] in
-      let h2_body = l_have "h2" (l_prf ("(⊥ ⇒c " ^ t ^ ")")) 
-        [l_assume ["nnf"]; l_apply ["⊥e"]; 
-         l_apply ["nnf"]; l_assume ["pf"]; l_refine ["pf"]] in
-      l_proof_statement (l_prfc ("(" ^ t ^ " =c ⊥)")) 
-        [l_assume ["nh"]; l_refine ["nh"; "_"]; h1_body; h2_body; 
-         l_assume ["s1"; "left1"]; l_apply ["left1"; "h1"; "h2"]] in 
+      let h1_footer =
+        (*final steps for obtaining contradiction (⊥)*) 
+        List.fold_left aux_foo "refine f2 _; \n" (List.init ((i2 - i1) / 2) (fun r -> r + 1)) ^ "refine f1; \n" in
+      (*finishing the proof*)
+        l_proof_statement (l_prfc (leqc t lbot)) 
+        [l_assume ["nh"]; l_refine ["nh"; "_"]; 
+        l_have "h1" (l_prf (limpliesc t lbot)) 
+          [l_assume ["h3"; "nf"]; l_refine ["h3"; "_"]; 
+          l_assume ["h4"]; have_f1; have_f2; h1_footer;]; 
+        l_have "h2" (l_prf (limpliesc lbot t)) 
+          [l_assume ["nnf"]; l_apply ["⊥e"]; 
+          l_apply ["nnf"]; l_assume ["pf"]; l_refine ["pf"]]; 
+        l_assume ["s1"; "left1"]; 
+        l_apply ["left1"; "h1"; "h2"]] in 
       proof_of_contradiction
-    | Ast.Symbol "true" -> let rec truth_counter = function 
+    | Symbol "true" -> let rec truth_counter = function 
       | [] -> 0 
       | x :: t -> (if x = Symbol "true" then 1 else 0) + truth_counter t in
       if len_xlist <> (truth_counter xlist) then raise (SemanticError ("Can't equate the LHS to " ^ ltop)) else 
-         l_proof_statement (l_prfc "(" ^ t ^ " =c " ^ ltop ^ ")") [
+         l_proof_statement (l_prfc (leqc t ltop)) [
           l_assume ["nh"]; l_refine ["nh _"]; 
-          l_have "h1" (l_prf ("(" ^ t ^ " ⇒c " ^ ltop ^ ")")) [
+          l_have "h1" (l_prf (limpliesc t ltop)) [
             l_assume ["h3"; "nt"]; l_refine ["∧e1c _ _ h3 nt"];
           ];
-          l_have "h2" (l_prf ("(" ^ ltop ^ " ⇒c " ^ t ^ ")")) [
+          l_have "h2" (l_prf (limpliesc ltop t)) [
             l_assume ["pt"];
-            l_refine [apply_n_times (len_xlist - 1) "∧ic _ _" "pt"]
+            l_refine [apply_n_times (len_xlist - 1) "∧ic _ _ pt" "pt"]
           ];
           l_assume ["s1"; "left1"];
           l_apply ["left1"; "h1"; "h2"] 
@@ -234,24 +181,62 @@ let rule_and_simplify x y =
   | _ -> raise (SyntaxError "LHS of the equality must be of type Ast.And")
 
 
+let predefined_proof_generator (ast_term : term) (ref : l_term list) : l_term = 
+  (*uses predefined lambdapi proofs directly*)
+  l_proof_statement (l_prfc @@ term_translate @@ ast_term) [l_refine ref]
+
+
 let step_translate cl rule premises =
   match rule with 
   | "and_simplify" -> let lpi_proof = match cl with 
-    | [Ast.Equal (x, y)] -> rule_and_simplify x y
+    | [Equal (x, y)] -> rule_and_simplify x y
     | _ -> raise (SyntaxError "expected a clause with only one literal of type Ast.Equal")
     in lpi_proof
   | "th_resolution" -> let lpi_proof = match premises with 
     | None -> raise (SyntaxError "need premises for this rule")
-    | Some (Ast.Annotation _) -> "/* TODO */"
+    | Some (Annotation _) -> "/* TODO */"
     | _ -> raise (SyntaxError "premises must be of type Ast.Annotation")
     in lpi_proof 
-  | "equiv_pos2" -> "/* TODO */"
-  | "false" -> "/* TODO */"
+  | "equiv_pos2" -> let lpi_proof = match cl with 
+    | [Not (Equal (x, y)); Not a; b] -> if x = a && y = b then
+      predefined_proof_generator (Or cl) [rule; "_ _";]
+      else raise (SyntaxError "clause is not of the required form") 
+    | _ -> raise (SyntaxError "clause is not of the required form")
+    in lpi_proof
+  | "not_not" -> let lpi_proof = match cl with 
+    | [Not (Not (Not (x))); y] -> if x = y then 
+      l_proof_statement (l_prfc @@ term_translate @@ (Or cl)) [l_refine ["not_not _ _"]] 
+      else raise (SyntaxError "clause is not of the required form") 
+    | _ -> raise (SyntaxError "clause is not of the required form")
+    in lpi_proof
+  | "eq_reflexive" -> let lpi_proof = match cl with 
+    | [Equal (x, y)] -> if x = y then 
+      l_proof_statement (l_prfc @@ term_translate @@ Equal (x, y)) [l_refine ["eq_reflexive _"]]
+      else raise (SyntaxError "")
+    | _ -> raise (SyntaxError "")
+    in lpi_proof
+  | "implies" -> let lpi_proof = match cl, premises with 
+    | [Not _; _], Some (Annotation ([premise1], [])) -> predefined_proof_generator (Or cl) [rule; "_ _"; premise1]
+    | _ -> raise (SyntaxError "clause or premises are not of the required form") in lpi_proof
+  | "not_implies1" | "not_implies2" -> let lpi_proof = match cl, premises with 
+    | [phi], Some (Annotation ([premise1], [])) -> predefined_proof_generator phi [rule; "_ _" ;premise1]
+    | _ -> raise (SyntaxError "clause or premises are not of the required form") in lpi_proof
+  | "equiv1" | "equiv2" | "not_equiv1" | "not_equiv2" 
+  | "xor1" | "xor2" | "not_xor1" | "not_xor2" -> let lpi_proof = match cl, premises with 
+    | [_; _], Some (Annotation ([premise1], [])) -> predefined_proof_generator (Or cl) [rule; "_ _"; premise1]
+    | _ -> raise (SyntaxError "clause or premises are not of the required form") in lpi_proof
+  | "implies_neg1" | "implies_neg2" -> let lpi_proof = match cl with 
+    | [_; _] -> predefined_proof_generator (Or cl) [rule; "_ _"]
+    | _ -> raise (SyntaxError "clause or premises are not of the required form") in lpi_proof
+  | "equiv_pos1" | "equiv_neg1" | "equiv_neg2" -> let lpi_proof = match cl with 
+    | [_; _; _] -> predefined_proof_generator (Or cl) [rule; "_ _"]
+    | _ -> raise (SyntaxError "clause or premises are not of the required form") in lpi_proof
+  | "true" | "false" -> predefined_proof_generator (List.hd cl) [rule]
   | "" -> raise (SyntaxError "missing rule")
   | _ -> "/* TODO */"
 
 let get_lp_commands = function
-| Assume x -> "symbol " ^ x.name ^ " ≔ Prfc (" ^ term_translate x.term ^ ");"   (* ≔ *)
+| Assume x -> "symbol " ^ x.name ^ " : Prfc (" ^ term_translate x.term ^ ");"   (* ≔ *)
 | Step x -> let proof_def =
   match step_translate x.clause x.rule x.annotations with 
   | "/* TODO */" -> "/* TODO */" 
